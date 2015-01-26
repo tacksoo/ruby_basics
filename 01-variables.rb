b8a5cf45614a8645c0757c739099c52453cb317d
tacksoo <tacksoo.im@gmail.com>
2015-01-26 03:08:38 +0000

    added bits on vars, regex, control structs

---
+++ 01-variables.rb
@@ -0,0 +1,49 @@
+# this is a ruby comment
+=begin
+  this is a multi-line comment
+  yay, more space for comment
+  and more comment
+=end
+
+# fixnum 
+n = 42
+
+# float
+f = 4.2
+
+# everything is an object in Ruby
+puts 42.equal? 40
+
+# to find out the type of a variable, use the class() method
+42.class
+
+# find all methods of a variable
+42.methods
+
+# boolean 
+b = true # or false
+
+# string
+s = "ruby is cool"
+
+# string interpolation
+puts "#{s}, ain't it?"
+
+# string formatting 
+puts "Gwinnett has 6%% tax rate: e.g. $2.50 bill comes to $%.2f %s" % [ 2.5*0.06+2.5, "total" ]
+
+# arrays
+a = [ 1, 2, 3 ]
+
+# hashes
+h = [ :one => "uno", :two => "dos", :thress => "tres" ]
+
+# the following is the same of above 
+h = [ one: "uno", two: "dos", three: "tres" ]
+
+
+
+
+
+
+ 
---
+++ 03-control.rb
@@ -0,0 +1,16 @@
+
+# if-statement
+if true
+    puts "it's true"
+end
+
+# you can use this other form 
+puts "it's true again!" if true
+
+rainy = false
+
+# if not rainy
+unless rainy  
+  puts "it is not raining outside!"
+end
+
