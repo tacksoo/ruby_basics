8668b46287fa7d76858ae9e569488790f09f8290
tacksoo <tacksoo.im@gmail.com>
2015-02-20 21:12:17 -0500

    class variables, methods, and constants

--- 11-subclass.rb
+++ 11-subclass.rb
@@ -2,8 +2,13 @@
 require_relative '07-classes.rb'
 
 module GameDepot
+  # constant
+  CORPORATE_SLOGAN = "You can play it. We can help."
 
   class UsedGame < Game
+    # class variable
+    @@slogan = "All used games come with limited warranties"
+
     attr_accessor :condition
     
     def initialize(title,price,genre,condition)
@@ -23,11 +28,17 @@ module GameDepot
       end
     end
    
+    # class method, self keyword can be replaced with the class name
     def self.warranty(condition)
         return "One Month" if condition == "Like New" 
         return "Two Month" if condition == "Very Good" 
         return "Three Month" if condition == "Good" 
         return "Four Month" if condition == "Acceptable" 
+    end
+
+    # reader for class variable
+    def self.get_slogan
+      @@slogan
     end
   end
   
